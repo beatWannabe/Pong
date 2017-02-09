e11d24eced107f6767351553ad9f991b122349f9
beatWannabe <akumaokami10@gmail.com>
2017-02-09 18:04:56 -0500

    ponk game v1.5

---
+++ Ponk/src/ponk/game/pojo/PonkBall.java
@@ -0,0 +1,92 @@
+package ponk.game.pojo;
+
+import ponk.game.app.PlayGame;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author pdgomezl
+ */
+public class PonkBall {
+    
+    private static final int DIAMETER = 25;
+    private int ejeX = 1;
+    private int ejeY = 2;
+    private int aumentoEjeX = 1;
+    private int aumentoEjeY = 1;
+    private final PlayGame game;
+    
+    public PonkBall(PlayGame game) {
+        this.game = game;
+    }
+    
+    public void move() throws InterruptedException {
+        if (ejeX + aumentoEjeX < 0) {
+            aumentoEjeX = 1;
+        }
+        if (ejeX + aumentoEjeX > game.getWidth() - DIAMETER) {
+            aumentoEjeX = -1;
+        }
+        if (ejeY + aumentoEjeY < 0) {
+            aumentoEjeY = 1;
+        }
+        if (ejeY + aumentoEjeY > game.getHeight() - DIAMETER) {
+            /*ejeX = 0;
+            ejeY = 0;*/
+            game.gameOver();
+        }
+        if (collision()) {
+            Thread.sleep(15);
+            aumentoEjeY = -1;
+            ejeY = game.bar.getTopY() - DIAMETER;
+        }
+        ejeX = ejeX + aumentoEjeX;
+        ejeY = ejeY + aumentoEjeY;
+    }
+    
+    private boolean collision() {
+        return game.bar.getBounds().intersects(getBounds());
+    }
+    
+    public void paint(Graphics2D g) {
+        g.fillOval(ejeX, ejeY, DIAMETER, DIAMETER);
+    }
+    
+    public Rectangle getBounds() {
+        return new Rectangle(ejeX, ejeY, DIAMETER, DIAMETER);
+    }
+    
+    public int getEjeX() {
+        return ejeX;
+    }
+    
+    public void setEjeX(int ejeX) {
+        this.ejeX = ejeX;
+    }
+    
+    public int getEjeY() {
+        return ejeY;
+    }
+    
+    public void setEjeY(int ejeY) {
+        this.ejeY = ejeY;
+    }
+    
+    public int getAumentoEjeX() {
+        return aumentoEjeX;
+    }
+    
+    public void setAumentoEjeX(int aumentoEjeX) {
+        this.aumentoEjeX = aumentoEjeX;
+    }
+    
+    public int getAumentoEjeY() {
+        return aumentoEjeY;
+    }
+    
+    public void setAumentoEjeY(int aumentoEjeY) {
+        this.aumentoEjeY = aumentoEjeY;
+    }
+    
+}
---
+++ Ponk/src/ponk/game/pojo/PonkBar.java
@@ -0,0 +1,86 @@
+package ponk.game.pojo;
+
+import ponk.game.app.PlayGame;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+
+/**
+ *
+ * @author pdgomezl
+ */
+public class PonkBar {
+
+    private static final int NIVEL_AVANCE_BALL = 2;
+    private static int ejeY;
+    private static final int WITH = 60;
+    private static final int HEIGHT = 10;
+    private int ejeX = 205;
+    private int aumentoEjeX;
+    private final PlayGame game;
+
+    public PonkBar(PlayGame game) {
+        this.game = game;
+    }
+
+    public void move() {
+        if (ejeX + aumentoEjeX > 0 && ejeX + aumentoEjeX < game.getWidth() - WITH) {
+            ejeX = ejeX + aumentoEjeX * NIVEL_AVANCE_BALL;
+        }
+    }
+
+    public void paint(Graphics2D g) {
+        ejeY = game.getHeight() - 60;
+        g.fillRect(ejeX, ejeY, WITH, HEIGHT);
+    }
+
+    public void keyReleased(KeyEvent e) {
+        aumentoEjeX = 0;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        //----------CON LAS FLECHAS
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_LEFT:
+                aumentoEjeX = -1;
+                break;
+            //---------CON A O D
+            case KeyEvent.VK_A:
+                aumentoEjeX = -1;
+                break;
+            case KeyEvent.VK_RIGHT:
+                aumentoEjeX = 1;
+                break;
+            case KeyEvent.VK_D:
+                aumentoEjeX = 1;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(ejeX, ejeY, WITH, HEIGHT);
+    }
+
+    public int getTopY() {
+        return ejeY - HEIGHT;
+    }
+
+    public int getEjeX() {
+        return ejeX;
+    }
+
+    public void setEjeX(int ejeX) {
+        this.ejeX = ejeX;
+    }
+
+    public int getAumentoEjeX() {
+        return aumentoEjeX;
+    }
+
+    public void setAumentoEjeX(int aumentoEjeX) {
+        this.aumentoEjeX = aumentoEjeX;
+    }
+
+}
